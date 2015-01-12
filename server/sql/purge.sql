c024f6a3e5e2a58a13aed9be1bee739bb8b1943e
Kory Prince <korylprince@gmail.com>
2015-01-12 15:59:10 -0600

    fix some bugs and timespans on purge.sql

--- server/sql/purge.sql
+++ server/sql/purge.sql
@@ -2,20 +2,26 @@ CREATE TEMPORARY TABLE tmp_ids (
     id INT NOT NULL PRIMARY KEY
 );
 
-/* First Week -> Untouched */
-INSERT INTO tmp_ids SELECT id FROM chronicle WHERE time > NOW() - INTERVAL 1 WEEK;
+/* First Day -> Untouched */
+INSERT INTO tmp_ids SELECT id FROM chronicle WHERE time > NOW() - INTERVAL 1 DAY;
+
+/* First Week -> Keep first/last record for each hour per data set */ 
+INSERT IGNORE INTO tmp_ids SELECT ids.id FROM (
+    SELECT Min(id) as id FROM chronicle GROUP BY DATE_FORMAT(time,'%Y-%m-%d-%H:00:00'), username, serial, ip, internetip
+    UNION SELECT Max(id) as id FROM chronicle GROUP BY DATE_FORMAT(time,'%Y-%m-%d-%H:00:00'), username, serial, ip, internetip
+) as ids JOIN chronicle on ids.id = chronicle.id WHERE chronicle.time > NOW() - INTERVAL 1 WEEK;
 
 /* First Month -> Keep first/last record for each day per data set */ 
-INSERT IGNORE INTO tmp_ids SELECT id FROM (
+INSERT IGNORE INTO tmp_ids SELECT ids.id FROM (
     SELECT Min(id) as id FROM chronicle GROUP BY DATE(time), username, serial, ip, internetip
     UNION SELECT Max(id) as id FROM chronicle GROUP BY DATE(time), username, serial, ip, internetip
-) as ids WHERE time > NOW() - INTERVAL 1 MONTH;
+) as ids JOIN chronicle on ids.id = chronicle.id WHERE chronicle.time > NOW() - INTERVAL 1 MONTH;
 
 /* First Year -> Keep first/last record for each week per data set */ 
-INSERT IGNORE INTO tmp_ids SELECT id FROM (
+INSERT IGNORE INTO tmp_ids SELECT ids.id FROM (
     SELECT Min(id) as id FROM chronicle GROUP BY WEEK(time), username, serial, ip, internetip
     UNION SELECT Max(id) as id FROM chronicle GROUP BY WEEK(time), username, serial, ip, internetip
-) as ids WHERE time > NOW() - INTERVAL 1 YEAR;
+) as ids JOIN chronicle on ids.id = chronicle.id WHERE chronicle.time > NOW() - INTERVAL 1 YEAR;
 
 /* Everything Else -> Keep first/last record for each month per data set */ 
 INSERT IGNORE INTO tmp_ids SELECT id FROM (
